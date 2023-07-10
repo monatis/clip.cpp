2cbeb8e51910803085c76de3bd43b44e686b4f71
Green Sky <green@g-s.xyz>
2023-07-10 16:50:18 +0200

    update usearch dep to v0.20.0 fixes reported memory leak https://github.com/unum-cloud/usearch/issues/138

--- examples/image-search/search.cpp
+++ examples/image-search/search.cpp
@@ -133,7 +133,7 @@ int main(int argc, char** argv) {
     printf("search results:\n");
     printf("distance path\n");
     for (std::size_t i = 0; i != results.size(); ++i) {
-        printf("  %f %s\n", results[i].distance, image_file_index.at(results[i].element.label).c_str());
+        printf("  %f %s\n", results[i].distance, image_file_index.at(results[i].member.label).c_str());
     }
 
     clip_free(clip_ctx);
