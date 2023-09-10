2de5373b76f02f71f73651b56d768a5e5145a03f
M. Yusuf Sarıgöz <yusufsarigoz@gmail.com>
2023-09-10 17:22:59 +0300

    Python refactoring and additions to readme

--- examples/python_bindings/clip_cpp/clip.py
+++ examples/python_bindings/clip_cpp/clip.py
@@ -314,34 +314,3 @@ class Clip:
 
     def __del__(self):
         clip_free(self.ctx)
-
-
-if __name__ == "__main__":
-    import argparse
-
-    ap = argparse.ArgumentParser(prog="clip")
-    ap.add_argument("-m", "--model", help="path to GGML file", required=True)
-    ap.add_argument(
-        "-v",
-        "--verbosity",
-        type=int,
-        help="Level of verbosity. 0 = minimum, 2 = maximum",
-        default=0,
-    )
-    ap.add_argument("-t", "--text", help="text to encode", required=True)
-    ap.add_argument("-i", "--image", help="path to an image file", required=True)
-    args = ap.parse_args()
-
-    clip = Clip(args.model, args.verbosity)
-
-    tokens = clip.tokenize(args.text)
-    text_embed = clip.encode_text(tokens)
-
-    image_embed = clip.load_preprocess_encode_image(args.image)
-
-    score = clip.calculate_similarity(text_embed, image_embed)
-
-    # Alternatively, you can just do:
-    # score = clip.compare_text_and_image(text, image_path)
-
-    print(f"Similarity score: {score}")
