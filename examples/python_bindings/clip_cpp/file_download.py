b6f63855e9d0704fafb4d09b413718da9d67f066
M. Yusuf Sarıgöz <yusufsarigoz@gmail.com>
2023-09-18 14:09:39 +0300

    python: List models on HF Hub (#68)
    * rm __pycache__
    
    * gitignore __pycache__
    
    * gitignore dist
    
    * fix: use find_library instead of assuming library extension
    
    * Output all libraries in the root build dir
    
    * Add scripts
    
    * Install with cmake
    
    * python: list models
    
    * python: fix find_library
    
    * python: list repos and models in a repo
    
    ---------
    
    Co-authored-by: denis-ismailaj <denis.ismailaj1@gmail.com>

--- examples/python_bindings/clip_cpp/file_download.py
+++ examples/python_bindings/clip_cpp/file_download.py
@@ -7,6 +7,29 @@ from typing import Optional, List, Dict, TypedDict, Union
 from .exceptions import RepositoryNotFoundError, RepositoryFileNameNotFound
 
 
+class Model:
+    def __init__(
+        self,
+        _id: str,
+        id: str,
+        likes: int,
+        private: bool,
+        downloads: int,
+        tags: List[str],
+        modelId: str,
+    ):
+        self._id = _id
+        self.id = id
+        self.likes = likes
+        self.private = private
+        self.downloads = downloads
+        self.tags = tags
+        self.modelId = modelId
+
+    def __str__(self):
+        return f"repo_id: {self.modelId}"
+
+
 class BlobLfsInfo(TypedDict, total=False):
     size: int
     sha256: str
@@ -33,7 +56,6 @@ class RepoFile:
 
 
 class ModelInfo:
-
     def __init__(
         self,
         *,
@@ -49,14 +71,14 @@ class ModelInfo:
         securityStatus: Optional[Dict] = None,
         **kwargs,
     ):
-
         self.modelId = modelId
         self.sha = sha
         self.lastModified = lastModified
         self.tags = tags
         self.pipeline_tag = pipeline_tag
-        self.siblings = [RepoFile(**x)
-                         for x in siblings] if siblings is not None else []
+        self.siblings = (
+            [RepoFile(**x) for x in siblings] if siblings is not None else []
+        )
         self.private = private
         self.author = author
         self.config = config
@@ -75,9 +97,9 @@ def model_info(
     repo_id: str,
     files_metadata: bool = False,
 ) -> ModelInfo:
-
-    get_files_metadata = urllib.parse.urlencode(
-        {'blobs': files_metadata}) if files_metadata else ""
+    get_files_metadata = (
+        urllib.parse.urlencode({"blobs": files_metadata}) if files_metadata else ""
+    )
     url = f"https://huggingface.co/api/models/{repo_id}/?" + get_files_metadata
     try:
         response = urllib.request.urlopen(url)
@@ -86,7 +108,7 @@ def model_info(
     except urllib.error.HTTPError as e:
         if e.code == 401:
             raise RepositoryNotFoundError
-        elif e.peek().decode('utf-8', errors='ignore') == "Entry not found":
+        elif e.peek().decode("utf-8", errors="ignore") == "Entry not found":
             raise RepositoryFileNameNotFound
         else:
             print(f"\nError getting Info about the repo_id: {e}")
@@ -95,18 +117,15 @@ def model_info(
         print(f"\nError getting Info about the repo_id: {e}")
 
 
-def model_download(
-    repo_id: str,
-    file_name: Union[str, List[str]]
-) -> str:
-    ''' Download HF model and returns the Path of the Downloaded file'''
+def model_download(repo_id: str, file_name: Union[str, List[str]]) -> str:
+    """Download HF model and returns the Path of the Downloaded file"""
 
-    # create a model dirictory to save the files neatly in one folder
-    models_dir = Path('./models/')
+    # create a model directory to save the files neatly in one folder
+    models_dir = Path("./models/")
     models_dir.mkdir(exist_ok=True)
     destination_path = models_dir.joinpath(file_name)
 
-    url = f'https://huggingface.co/{repo_id}/resolve/main/{file_name}'
+    url = f"https://huggingface.co/{repo_id}/resolve/main/{file_name}"
 
     def reporthook(count, block_size, total_size):
         # Calculate the progress
@@ -114,24 +133,23 @@ def model_download(
         progress = (downloaded_chunk / total_size) * 100
 
         # print(downloaded_chunk // total_size)
-        bar = ''.join(['=' if i <= progress/2 else ' ' for i in range(50)])
+        bar = "".join(["=" if i <= progress / 2 else " " for i in range(50)])
         sys.stdout.write(
-            f"\r[{bar}] {progress:.1f}% ({downloaded_chunk/1024**2:.2f} MB/{total_size/1024**2:.0f} MB)")
+            f"\r[{bar}] {progress:.1f}% ({downloaded_chunk/1024**2:.2f} MB/{total_size/1024**2:.0f} MB)"
+        )
         sys.stdout.flush()
 
     try:
         print(f"[File Info] {destination_path}")
         # check if the file exists and matches the size of that in the network
-        network_file_size = (urllib.request
-                             .urlopen(url)
-                             .info().get('Content-Length', 0))
-        if destination_path.is_file() \
-                and destination_path.stat().st_size == int(network_file_size):
+        network_file_size = urllib.request.urlopen(url).info().get("Content-Length", 0)
+        if destination_path.is_file() and destination_path.stat().st_size == int(
+            network_file_size
+        ):
             # raise FileNameAlreadyExists
             return models_dir
 
-        urllib.request.urlretrieve(
-            url, destination_path, reporthook=reporthook)
+        urllib.request.urlretrieve(url, destination_path, reporthook=reporthook)
         sys.stdout.write("\n")
         print(f"File downloaded to {destination_path}")
         return models_dir
@@ -139,10 +157,53 @@ def model_download(
     except urllib.error.HTTPError as e:
         if e.code == 401:
             raise RepositoryNotFoundError
-        elif e.peek().decode('utf-8', errors='ignore') == "Entry not found":
+        elif e.peek().decode("utf-8", errors="ignore") == "Entry not found":
             raise RepositoryFileNameNotFound
         else:
             print(f"\nError downloading file: {e}")
 
     except Exception as e:
         print(f"\nError downloading file: {e}")
+
+
+def get_models() -> List[Model]:
+    url = f"https://huggingface.co/api/models?filter=clip.cpp"
+    try:
+        response = urllib.request.urlopen(url)
+        data = json.load(response)
+        return [Model(**item) for item in data]
+    except urllib.error.HTTPError as e:
+        print(f"\nError listing available models: {e}")
+
+    except Exception as e:
+        print(f"\nError listing available models: {e}")
+
+
+def available_models():
+    if len(sys.argv) > 1 and sys.argv[-1] != "clip-cpp-models":
+        repo_id = sys.argv[-1]
+        repo = model_info(repo_id=repo_id, files_metadata=True)
+        print(f"Available models in repo {repo_id}:")
+        for model in repo.siblings:
+            if model.rfilename.endswith(".bin"):
+                name = model.rfilename
+                size = model.size / (1024 * 1024)
+                print(f"    model: {name} ({size:.2f} MB)")
+
+        return
+
+    models = get_models()
+    print(
+        "Below are available models on HuggingFace Hub that you can use with the clip-cpp package.\n"
+    )
+    print("Available models:")
+    for model in models:
+        print(model)
+
+    print(
+        "\nYou can pass one of the repo IDs above directly to `Clip()`, and it will download the smallest model in that repo automatically."
+    )
+    print(
+        "Alternatively, you can choose which to load from that repo by passing a value to `model_file` argument."
+    )
+    print("\nTo see model files in a repo, type `clip-cpp-models <repo_id>`")
